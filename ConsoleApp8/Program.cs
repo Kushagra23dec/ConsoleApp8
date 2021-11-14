343aa8f9af4e8b3faca19133da15db01507f06fb
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-14 20:07:38 +0530

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp8
+{   
+    class P
+    {
+        int age;
+        string name;
+
+       public P()
+        {
+            age = 21;
+            name = "Kushagra";
+        }
+        public string toString()
+        {
+            return age + " " + name;
+        }
+
+        override
+        public string ToString()
+        {
+            return age + "  " + name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            P p = new P();
+
+            Console.WriteLine("*****************************************************************");
+
+            Console.WriteLine(p);
+        }
+    }
+}
